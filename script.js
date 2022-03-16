fca229ee1109946fd89c21870502cb1df3ace963
syed abdullah ali <syedabdullahali380@gmail.com>
2022-03-17 00:56:31 +0530

    jsUpdate

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 
 let score = 20;
-
 let HighScore = 0;
-
 let secratNumber = Math.trunc(Math.random() * 20) + 1
 // this function build for messages  
 
 const displayMessage = (message) => {
     document.querySelector('.message').textContent = message
 }
-
 const Background_colorfunction = (containerstyle, boxStyel, bodycolor, Lastboxcolor, again_button_backgoundcolor, again_color) => {
     document.querySelector('#container').style.backgroundColor = containerstyle
     document.querySelector('#box').style.backgroundColor = boxStyel
     document.querySelector('body').style.color = bodycolor
-
     document.querySelector('#last-box').style.backgroundColor = Lastboxcolor
     const Again = document.querySelector('#Again')
     Again.style.backgroundColor = again_button_backgoundcolor
     Again.style.color = again_color
-
-
 }
 
 // this function for box style 
@@ -40,8 +34,6 @@ const score_update = (score) => {
     document.querySelector('.score').textContent = score
 }
 
-
-
 const input = document.querySelector('.guess')
 document.querySelector('#check').addEventListener('click', function () {
     if (!input.value) {
@@ -50,10 +42,9 @@ document.querySelector('#check').addEventListener('click', function () {
         let number = secratNumber
         document.querySelector('#box').textContent = number
         displayMessage('🥳 Correct  Number')
-        box_style_Width('150px','3px solid White')
+        box_style_Width('150px', '3px solid White')
         Background_colorfunction("rgba(252, 203, 113, 0.972)", '#D2691E', 'white', '#D2691E', '#D2691E', 'White')
 
-        
         // this statement for highscore
         if (score > HighScore) {
             HighScore = score
@@ -63,14 +54,11 @@ document.querySelector('#check').addEventListener('click', function () {
 
     } else if (input.value !== secratNumber) {
         if (score > 1) {
-
             displayMessage(input.value > secratNumber ? '📈 Too High!' : '📉 Too Low!')
             score--
             score_update(score)
-
         } else {
             score--
-            
             score_update(" 💥you lost the game ")
             box_style_Width('150px', '3px solid White')
             check_buttonfunction('rgb(220,20,60)', 'white')
@@ -78,11 +66,10 @@ document.querySelector('#check').addEventListener('click', function () {
         }
     }
 })
-
 // this is for reset and play again  game 
 
 const Reset_Button = document.querySelector('#Again')
-Reset_Button.addEventListener('click', function (){
+Reset_Button.addEventListener('click', function () {
     score = 20
     displayMessage('Start Guessing...')
     score_update(score)
@@ -93,13 +80,3 @@ Reset_Button.addEventListener('click', function (){
     Background_colorfunction("antiquewhite", 'antiquewhite', 'black', 'aqua', 'aqua', 'black')
     check_buttonfunction('antiquewhite', 'black')
 })
-
-
-
-
-
-
-
-
-
-
